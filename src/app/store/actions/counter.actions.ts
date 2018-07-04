cdf6cec0911f470da7d6dd1a0ddce762c2e38bf6
Gregor Doroschenko <g.doroschenko@gmail.com>
2018-07-04 15:38:42 +0200

    add new increment by value action

--- src/app/store/actions/counter.actions.ts
+++ src/app/store/actions/counter.actions.ts
@@ -4,6 +4,7 @@ export enum CounterActionTypes {
   INCREMENT = '[Counter] Increment',
   DECREMENT = '[Counter] Decrement',
   RESET = '[Counter] Reset',
+  INCREMENT_BY_VALUE = '[Counter] Increment by value'
 }
 
 export class Increment implements Action {
@@ -20,4 +21,10 @@ export class Reset implements Action {
   constructor(public payload: number) {}
 }
 
-export type CounterActionsUnion = Increment | Decrement | Reset;
+export class IncrementByValue implements Action {
+  readonly type = CounterActionTypes.INCREMENT_BY_VALUE;
+
+  constructor(public payload: number) {}
+}
+
+export type CounterActionsUnion = Increment | Decrement | Reset | IncrementByValue;
